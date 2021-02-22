0da71cb26800043d432cde023eb3b816470c5491
Pyatar <legoboy.kr2005@gmail.com>
2021-02-22 17:51:21 +0200

    MAKS

---
+++ ls029/main.js
@@ -0,0 +1,28 @@
+// let x = 2
+// let y  = 4
+// let z = 5
+// let c = x + y + z 
+
+// console.log(c)
+
+let myAge = 15
+let myName = 'MAKS'
+
+console.log(myAge, "-", )
+
+// alert("I AM MAKS")
+// alert("IT IS TRUE")
+let userName1 = prompt("КАК ТЕБЯ ЗОВУТ")
+let userName2 = prompt("Привет 'maks' ГДЕ ТИ РАБОТАЕШЬ/УЧИШСЯ")
+let userName3 = prompt("СКОЛЬКО ТЕБЕ ЛЕТ")
+let userName4 = prompt("ЛЮБИМІЙ ЦВЕТ")
+let userName5 = prompt("ИМЯ ЛУЧШЕГО ДРУГА")
+let userName6 = prompt("ИМЯ - maks  Место учеби - КПЛ  Возраст - 15 лет  Любимий цвет черний  Имя друга - Давид   ВСЕ ВЕРНО?")
+
+
+
+console.log(userName1)
+console.log(userName2)
+console.log(userName3)
+console.log(userName4)
+console.log(userName5)
---
+++ ls32/main.js
@@ -0,0 +1,23 @@
+// alert(" " + 1 + 0)
+// alert(true + false)
+// alert(6 / "3")
+// alert(4 + 5 + "px")
+// alert("px" + 5 +4)
+// alert("4" - 2)
+// let a = Number(prompt("Введите первое число"))
+//     b = Number(prompt("Введите второе число"))
+// alert("Сумма = " + (a+b))
+// alert(5 > 4)
+// alert("Ананас" > "Яблоко")
+// alert("2" > "12")
+// alert("Roman" < "Romanuch")
+//     aa = prompt("a = ")
+//     bb = prompt("b = ")
+// alert(aa > bb)
+let a = prompt("В каком году бил первій сценарий JS")
+    if(a==2015){
+    alert("Good")}
+    else if(a<2015){
+    alert("Чуть больше")}
+    else if(a>2015){
+    alert("Чуть больше")}
