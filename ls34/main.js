be7a24b00c84719654d780706ed5c1419598d60e
Pyatar <legoboy.kr2005@gmail.com>
2021-03-15 17:49:49 +0200

    MAX

---
+++ ls34/main.js
@@ -0,0 +1,8 @@
+// for(let i=1; i<51; 1++){
+//     alert(i++)
+//     console.log(i++)
+// }
+let i=0
+while(i<3){
+    alert(i++)
+}
---
+++ ls35/main.js
@@ -0,0 +1,20 @@
+// arr=['Roma','Katya','Ulia',"Max",'Получили','Подарок','не']
+//     alert(arr[0])
+//     alert(arr[3])
+//     alert(arr[4])
+//     alert(arr[5])
+
+// obj={'Kolia':'1000','Roma':'2000','Ulia':'50000'}
+// alert(obj['Ulia'])
+// arr={
+//     'Ulia':['30000','32000','40000'] ,
+//     'Roma':['10','8','6']
+// }
+// alert(arr['Ulia'][2])
+// arr={
+//     'boy':['Roma ','Max','Ilya','Timofei','Artem'],
+//     'girl':['Vika','Katia','Sasha','Ulia']
+// }
+// alert(arr['boy'][0] + arr['girl'][0])
+arr=[2,3,4,5]
+alert(arr[0]*arr[1]*arr[2]*arr[3])
