1fd346a9db054a48de67c466aca7863ba592691d
Pyatar <legoboy.kr2005@gmail.com>
2021-03-19 17:48:26 +0200

    MAX

---
+++ ls36/main.js
@@ -0,0 +1,46 @@
+// let a = (prompt('Введите число'))
+// if(a>0){
+//     a++
+//     alert(a)}
+// else {
+// alert(a)} 
+// первая задача
+// let a = prompt('Введите число')
+// if(a>0){
+//     a++
+//     alert(a)}
+//     else{alert(a-2)}
+//     Вторая задача 
+// let a = Number(prompt('Введите число'))
+//     if(a>0){
+//     a++
+//     alert(a)}
+//     else if(a==0){
+//         alert(10)}
+//     else{
+//         a += -2  
+//         alert(a)
+//     }
+// Третяя задача
+let a = prompt('Введите 1 числo')
+let b = prompt('Введите 2 числo')
+let c = prompt('Введите 3 числo')
+    if(a,b,c >0){
+        alert(0)
+    }
+    if(a,b,c <0){
+        alert(3)
+
+    }
+    if(a>0, b<0, c<0){
+        alert(1)
+    }
+    if(a>0, b>0, c<0){
+        alert(2)
+    }
+    if(a<0, b>0, c>0){
+        alert(2)
+    }
+    if(a<0, b>0, c<0){
+        alert(1)
+    }
